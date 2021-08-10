8937d0a2a052a2e069aa5a7ff6ad3b4bbb604be3
Cícero Augusto <47788319+Binecomestogetyou@users.noreply.github.com>
2021-08-10 14:07:41 -0300

    Add files via upload
    Estrutura básica da função principal foi implementada.
    
    Main function basic structure was implemented.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include"Peca.h"
+#include"Conjunto.h"
 
 int main(){
+    Conjunto Brancas = Conjunto(BRANCO), Pretas = Conjunto(PRETO);
     
+    while(true){
+    	Brancas.jogar();
+    	Pretas.jogar();
+    }    
 };
