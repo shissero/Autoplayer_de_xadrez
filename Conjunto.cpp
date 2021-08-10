fb85a962b5397b39b404b8831e311aba2d83a9bb
Cícero Augusto <47788319+Binecomestogetyou@users.noreply.github.com>
2021-08-10 14:43:10 -0300

    Add files via upload
    Header guard adicionado. Os construtores das classes filhas de Peca foram alterados para a nova sintaxe.
    
    Header guard added. Constructors to children classes of Peca were changed to the new syntax.

--- Conjunto.cpp
+++ Conjunto.cpp
@@ -12,47 +12,47 @@ using std::vector
 class Conjunto {
     vector<Peca> Conjunto;
     
-    Conjunto(const int COR):
-    	if(COR == Peca.BRANCO){
+    Conjunto(int COR):
+    	if(COR == BRANCO){
     		// Inicialize as peças brancas
     		
     		
     		for(int i = 1; i < 9; i++){
-    			Conjunto.insert(Peao(Posicao(i, 2), COR);
+    			Conjunto.insert(Peao(i, 2, COR);
     		}
     		
-    		Conjunto.insert(Torre(Posicao(1, 1), COR);
-    		Conjunto.insert(Torre(Posicao(8, 1), COR);
+    		Conjunto.insert(Torre(1, 1, COR));
+    		Conjunto.insert(Torre(8, 1, COR));
     		
-		Conjunto.insert(Cavalo(Posicao(2, 1), COR);
-		Conjunto.insert(Cavalo(Posicao(7, 1), COR);
+		Conjunto.insert(Cavalo(2, 1, COR));
+		Conjunto.insert(Cavalo(7, 1 COR));
 		
-		Conjunto.insert(Bispo(Posicao(3, 1), COR);
-		Conjunto.insert(Bispo(Posicao(6, 1), COR);
+		Conjunto.insert(Bispo(3, 1, COR));
+		Conjunto.insert(Bispo(6, 1, COR));
 		
-		Conjunto.insert(Dama(Posicao(4, 1), COR);
+		Conjunto.insert(Dama(4, 1, COR));
 		
-		Conjunto.insert(Rei(Posicao(5, 1), COR);
+		Conjunto.insert(Rei(5, 1, COR));
     	}
     	else{
     		// Inicialize as peças pretas
     		
     		
     		for(int i = 1; i < 9; i++){
-    			Conjunto.insert(Peao(Posicao(i, 7), COR);
+    			Conjunto.insert(Peao(i, 7, COR));
     		}
     		
-    		Conjunto.insert(Torre(Posicao(1, 8), COR);
-    		Conjunto.insert(Torre(Posicao(8, 8), COR);
+    		Conjunto.insert(Torre(1, 8, COR));
+    		Conjunto.insert(Torre(8, 8, COR));
     		
-		Conjunto.insert(Cavalo(Posicao(2, 8), COR);
-		Conjunto.insert(Cavalo(Posicao(7, 8), COR);
+		Conjunto.insert(Cavalo(2, 8, COR));
+		Conjunto.insert(Cavalo(7, 8, COR));
 		
-		Conjunto.insert(Bispo(Posicao(3, 8), COR);
-		Conjunto.insert(Bispo(Posicao(6, 8), COR);
+		Conjunto.insert(Bispo(3, 8, COR));
+		Conjunto.insert(Bispo(6, 8, COR));
 		
-		Conjunto.insert(Dama(Posicao(4, 8), COR);
+		Conjunto.insert(Dama(4, 8, COR));
 		
-		Conjunto.insert(Rei(Posicao(5, 8), COR);
+		Conjunto.insert(Rei(5, 8, COR));
     	}
 };
