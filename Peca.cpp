9667b8b2e39f10d4568dfc081542b5a7828bad2a
binecomestogetyou <cicero1995augustoo@gmail.com>
2021-10-30 19:57:09 -0300

    Atualização de arquivos

--- Peca.cpp
+++ Peca.cpp
@@ -4,11 +4,11 @@
 
 #include"Conjunto.h"
 #include"Peca.h"
-#include"Posicao.h"
 
+using namespace std;
 
 Peca::Peca(Posicao pos, int color)
-	: casa (pos), cor (color)
+	: posicao (pos), cor (color)
 	{}
 
 bool Peca::mover(){
@@ -18,25 +18,86 @@ bool Peca::mover(){
 	return false;
 }
 
-void Peca::gerarMovimentos(vector<Posicao *> movimentos){}
+void Peca::gerarMovimentos(vector<Posicao *> *movimentos){}
+
+void Peca::gerarMovimentosCardeais(Peca peca, vector<Posicao *> *movimentos){
+
+	// Gerar movimentos no sentido decrescente das linhas
+	if(peca.posicao.linha > 1){
+	
+		for(int i = peca.posicao.linha - 1; i > 0; i--) {
+		
+			Peca *aux = Conjunto::estaVazia( Posicao(peca.posicao.coluna, i) );
+			
+			if( aux != 0 && aux -> cor == peca.cor ) break;
+			else movimentos -> push_back( new Posicao(peca.posicao.coluna, i) );
+			
+		}
+		
+	}
+	
+	// Gerar movimentos no sentido crescente das linhas
+	if(peca.posicao.linha < 8 ){
+	
+		for(int i = peca.posicao.linha + 1; i < 9; i++){
+		
+			Peca *aux = Conjunto::estaVazia( Posicao(peca.posicao.coluna, i) );
+			
+			if( aux != 0 && aux -> cor == peca.cor ) break;
+			else movimentos -> push_back( new Posicao(peca.posicao.coluna, i) );
+			
+		}
+		
+	}
+	
+	// Gerar movimentos no sentido decrescente das colunas
+	if(peca.posicao.coluna > 1){
+	
+		for(int i = peca.posicao.coluna - 1; i > 0; i--){
+		
+			
+		
+			Peca *aux = Conjunto::estaVazia( Posicao(peca.posicao.coluna, i) );
+			
+			if( aux != 0 && aux -> cor == peca.cor ) break;
+			else movimentos -> push_back( new Posicao(i, peca.posicao.linha) );
+			
+		}
+		
+	}
+	
+	// Gerar movimentos no sentido crescente das colunas
+	if(peca.posicao.coluna < 8){
+	
+		for(int i = peca.posicao.coluna + 1; i < 9; i++){
+		
+			Peca *aux = Conjunto::estaVazia( Posicao(peca.posicao.coluna, i) );
+			
+			if( aux != 0 && aux -> cor == peca.cor ) break;
+			else movimentos -> push_back( new Posicao(i, peca.posicao.linha) );
+			
+		}
+		
+	}
+}
 		
 Posicao Peca::obterPosicao(){
-	return this->casa;
+	return this->posicao;
 }
 
 int Peca::obterCor(){
 	return this->cor;
 }
 
-std::string Peca::obterCorComoString(){
+string Peca::obterCorComoString(){
 	return this->cor == BRANCO ? "branco" : "preto";
 }
 
 void Peca::mudarPosicao(Posicao destino){
 
-	std::cout << this->casa.toString() << " para " << destino.toString() << "\n";	
+	cout << this->posicao.toString() << " para " << destino.toString() << "\n";	
 	
-	this->casa = destino;
+	this->posicao = destino;
 }
 
-std::string Peca::obterClasse(){}
+string Peca::obterClasse(){ return 0; }
--- Peca.h
+++ Peca.h
@@ -1,18 +1,39 @@
 #ifndef PECA_H_
 #define PECA_H_
+
 #include<array>
+#include<string>
+#include<vector>
+
+#include"Posicao.h"
 
 #define BRANCO 0
 #define PRETO 1
 
+using namespace std;
+
 class Peca {
 
 	public:
-		Peca(Posicao posicao, int cor);
-
-		virtual bool mover(Peca peca);
+		Peca(Posicao, int);
 		
-	private:
+		int obterCor();
+		
+		virtual void gerarMovimentos(vector<Posicao *> *);
+		
+		void gerarMovimentosCardeais(Peca, vector<Posicao *> *);
+		
+		string obterCorComoString();
+		
+		virtual string obterClasse();
+		
+		Posicao obterPosicao();
+
+		virtual bool mover();
+		
+		void mudarPosicao(Posicao);
+		
+	protected:
 	
 	int cor;
 	Posicao posicao;
